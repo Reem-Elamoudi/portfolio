8107718712ae4fe8ac042de98792c8bdafd2eaed
reem-elamoudi <116013466+reem-elamoudi@users.noreply.github.com>
2023-06-06 21:43:17 +0200

    test

--- main.js
+++ main.js
@@ -23,28 +23,30 @@ function closeList(){
 
 
 //landing animation 
-const landingItems = document.querySelectorAll("#landing .continer *:not(#landing .continer .content a)");
-const landingA = document.querySelector("#landing .continer .content a")
+window.onload = ()=>{
+	const landingItems = document.querySelectorAll("#landing .continer *:not(#landing .continer .content a)");
+	const landingA = document.querySelector("#landing .continer .content a")
 
-landingItems.forEach((item, index) => {
+	landingItems.forEach((item, index) => {
+		setTimeout(() => {
+			item.style.cssText = "top: 0; opacity: 1;";
+			landingA.style.opacity = "1"
+		}, index * 100);
+	});
 	setTimeout(() => {
-		item.style.cssText = "top: 0; opacity: 1;";
 		landingA.style.opacity = "1"
 	}, index * 100);
-});
-setTimeout(() => {
-	landingA.style.opacity = "1"
-}, index * 100);
+}
 
 
 
 
 //scroll animation
 window.onscroll = () => {
-const cards = document.querySelectorAll("#project .continer .cards .card .img");
-const spanProject = document.querySelectorAll("#project .continer .cards .card span");
-const projectP = document.querySelector("#project .continer p:not(#sectionTitle)");
-const work = document.querySelector("#project .continer");
+	const cards = document.querySelectorAll("#project .continer .cards .card .img");
+	const spanProject = document.querySelectorAll("#project .continer .cards .card span");
+	const projectP = document.querySelector("#project .continer p:not(#sectionTitle)");
+	const work = document.querySelector("#project .continer");
 
 	//project animation
   const topW = work.getBoundingClientRect().top;
@@ -75,14 +77,14 @@ const work = document.querySelector("#project .continer");
 
 	const topA = about.getBoundingClientRect().top;
 
-	if (topA <= vh) {
-		p.forEach((p, index) => {
-		setTimeout(() => {
-			p.style.cssText = "top: 0; opacity: 1;";
-			img.style.cssText = "max-width: 395px; opacity: 1;";
-		}, index * 50);
-	});
-}
+		if (topA <= vh) {
+			p.forEach((p, index) => {
+			setTimeout(() => {
+				p.style.cssText = "top: 0; opacity: 1;";
+				img.style.cssText = "max-width: 395px; opacity: 1;";
+			}, index * 50);
+		});
+	}
 
 
 	//contact animation
@@ -91,13 +93,13 @@ const work = document.querySelector("#project .continer");
 
 	const topC = contact.getBoundingClientRect().top;
 
-	if (topC <= vh) {
-		contactItems.forEach((item, index) => {
-      setTimeout(() => {
-        item.style.cssText = "top: 0; opacity: 1;";
-      }, index * 50);
-    });
-}
+		if (topC <= vh) {
+			contactItems.forEach((item, index) => {
+				setTimeout(() => {
+					item.style.cssText = "top: 0; opacity: 1;";
+				}, index * 50);
+			});
+	}
 
 
 };
@@ -108,35 +110,34 @@ const work = document.querySelector("#project .continer");
 
 
 // send email from form
-
 function sendEmail(){
-	const name = document.querySelector('#name');
-	const email = document.querySelector('#email');
-	const ms = document.querySelector('#ms');
-	const valid = document.querySelectorAll('.valid-ms');
+  const nameE = document.querySelector('#name');
+  const email = document.querySelector('#email');
+  const ms = document.querySelector('#ms');
+  const valid = document.querySelectorAll('.valid-ms');
 
-[name, email, ms].forEach(input => {
-	input.addEventListener('keypress', function(e) {
-		if (e.key === 'Enter') {
-			sendEmail();
-			reset();
-			e.preventDefault();
-		}
-	});
-})
-
-Email.send({
-	SecureToken : "905f8ba7-e52a-48da-9499-ace37e39e7ed",
-	To : 'rela69318am@gmail.com',
-	From : 'rela69318am@gmail.com',
-	Subject : "email from site:",
-	Body : `
-	NANE: ${name.value}<br>
-	EMAIL: ${email.value}<br>
-	MASSAGE: ${ms.value}
-	`
-	}).then(
-	message => alert("message sent successfully")
-	);
-
+  Email.send({
+    SecureToken : "905f8ba7-e52a-48da-9499-ace37e39e7ed",
+    To : 'rela69318am@gmail.com',
+    From : 'rela69318am@gmail.com',
+    Subject : "email from site:",
+    Body : `
+      NAME: ${nameE.value}<br>
+      EMAIL: ${email.value}<br>
+      MESSAGE: ${ms.value}
+    `
+  }).then(
+    message => alert("message sent successfully")
+  );
 }
+
+// Add event listener for Enter key press
+[nameE, email, ms].forEach(input => {
+  input.addEventListener('keypress', function(e) {
+    if (e.key === 'Enter') {
+      sendEmail();
+      reset();
+      e.preventDefault();
+    }
+  });
+});
